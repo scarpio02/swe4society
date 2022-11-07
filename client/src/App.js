9753c2884149623252d9be3c4e8f354ca6eb5eb6
scarpio02 <scarpio@ufl.edu>
2022-11-07 00:42:57 -0500

    login-backend-wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
+/*
 import React from "react";
  
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
+
 // We import all the components we need in our app
-import Navbar from "./components/navbar.js";
-import RecordList from "./components/recordList.js";
-import Edit from "./components/edit.js";
-import Create from "./components/create.js";
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
  
 const App = () => {
  return (
@@ -21,5 +22,30 @@ const App = () => {
    </div>
  );
 };
- 
+
 export default App;
+*/
+
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import { Register } from './components/register'
+import { Login } from './components/login'
+import { Profile } from './components/profile'
+
+function App() {
+  // <BrowserRouter>
+  //   <Routes>
+  //   <Route path="/" element={<App />}>
+  //     <Route element={Register} path="/register"/>
+  //     <Route element={Login} path="/login"/>
+      
+  //     </Route>
+  //   </Routes>
+  // </BrowserRouter>
+  <p>hello</p>
+}
+//<Route component={ProfilePage} exact path="/u/:userId" />
+
+
+export default App;
+
+
--- server/server.js
+++ server/server.js
@@ -7,13 +7,121 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+// THis is previous DB Connection
+
+// const dbo = require("./db/conn");
  
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
+// app.listen(port, () => {
+//   // perform a database connection when server starts
+//   dbo.connectToServer(function (err) {
+//     if (err) console.error(err);
  
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+//   });
+//   console.log(`Server is running on port: ${port}`);
+// });
+
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json();
+// app.use(jsonParser);
+// app.use(urlencodedParser);
+const Db = process.env.ATLAS_URI;
+
+mongoose.connect(Db, { useNewUrlParser:true, useUnifiedTopology:true })
+.then((res) => {
+  app.listen(process.env.PORT, () => console.log("Server is live"))
+  })
+.catch(err => console.log(err))
+
+
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const User = require("./models/user");
+
+app.post("/register", async (req, res) => {
+  const user = req.body;
+
+  console.log(req.body);
+  // check if the username or email has been taken by another user already
+  const takenUsername = await User.findOne({username: user.username})
+  const takenEmail = await User.findOne({email: user.email})
+
+  if (takenUsername || takenEmail) {
+    res.json({message: "Username or email has already been taken"})
+  } else {
+    user.password = await bcrypt.hash(req.body.password, 10)
+
+    const dbUser = new User({
+      username: user.username.toLowerCase(),
+      email: user.email.toLowerCase(),
+      password: user.password
+    })
+
+    dbUser.save()
+    res.json({message: "Success"})
+  }
+})
+
+app.post("/login", (req, res) => {
+    
+  const userLoggingIn = req.body;
+
+  User.findOne({username: userLoggingIn.username})
+  .then(dbUser => {
+    if (!dbUser) {
+      return res.json({
+        message: "Invalid Username or Password"
+      })
+    }
+    bcrypt.compare(userLoggingIn.password, dbUser.password)
+    .then(isCorrect => {
+      if (isCorrect) {
+        const payload = {
+          id: dbUser._id,
+          username: dbUser.username,
+        }
+        jwt.sign(
+          payload,
+          process.env.JWT_SECRET,
+          {expiresIn: 86400},
+          (err, token) => {
+            if (err) return res.json({message: err})
+            return res.json({
+              message: "Success",
+              token: "Bearer " + token
+            })
+          }
+        )
+      } else {
+        return res.json({
+          message: "Invalid Username or Password"
+        })
+      }
+    })
+  })
+})
+
+function verifyJWT(req, res, next) {
+  const token = req.headers["x-access-toke"]?.split(' ')[1]
+
+  if (token) {
+    jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+      if (err) return res.json({
+        isLoggedIn: false,
+        message: "Failed To Authenticate"
+      })
+      req.user = {};
+      req.user.id = decoded.id
+      req.user.username = decoded.username
+      next()
+    })
+  } else {
+    res.json({message: "Incorrect Token Given", isLoggedIn: false})
+  }
+}
+
+app.get("/getUsername", verifyJWT, (req, res) => {
+  res.json({isLoggedIn: true, username: req.user.username})
+})
