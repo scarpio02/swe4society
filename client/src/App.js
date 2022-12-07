7174b3f01d9c11fe16a45f8c71ef649d84a1aca0
Andrew Ruales <67757892+andrewruales@users.noreply.github.com>
2022-12-06 20:45:38 -0500

    cleaning out commented code
    deleted some headers and commented code below the end of the program

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,15 @@ import React from "react";
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
 import './index.css'
+
 // We import all the components we need in our app
 import Navbar from "./components/navbar.jsx";
-// import RecordList from "./components/recordList.js";
-// import Edit from "./components/edit.js";
-
 import Register from "./components/register.jsx";
 import Login from "./components/login.jsx";
 import Get from "./components/get.jsx";
  
 const App = () => {
  return (
-//   <div className="bg-gray-900 min-h-screen">
-//   <BrowserRouter>
-//       <Routes>
-//           <Route exact path="/"/>
-//           <Route component={Register} exact path="/register"/>
-//           <Route component={Login}  exact path="/login"/>
-//       </Routes>
-//   </BrowserRouter>
-// </div>
-
    <div className="bg-gray-900 min-h-screen">
      <Navbar />
      <Routes>
@@ -37,16 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-// const App = () => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Routes>
-//         <Route exact path="/" element={<RecordList />} />
-//         <Route path="/edit/:id" element={<Edit />} />
-//         <Route path="/register" element={<Register />} />
-//       </Routes>
-//     </div>
-//   );
-//  };
