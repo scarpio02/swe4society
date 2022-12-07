c8be741732f1bd3c6acb6249ff6006766bb36ea0
phillipsmax14 <51027129+phillipsmax14@users.noreply.github.com>
2022-12-06 22:28:02 -0500

    Update get.mjs

--- client/src/components/get.mjs
+++ client/src/components/get.mjs
@@ -1,34 +1,39 @@
-// const express = require('express')
-// const router = express.Router();
-// const foodpost = require('../modles/modelpost');
+
+//import of fetch, a function that allows API connection and return available
 import fetch from 'node-fetch';//require("node-fetch");
-//import App from './app.jsx'
+
 export async function GetNutrientInfo(params) {
 
 console.log('Hellovcfbhgjkl')
 
-//const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${encodeURIComponent(params.query)}&dataType=${encodeURIComponent(params.dateType)}&pageSize=${encodeURIComponent(params.pagesize)}`
-//const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=adGOkaniwDcX5OGdQwBKtAG4NnaCknGEsJrpcCX5`
+//API url, needed for connection to API
 const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${encodeURIComponent(params.query)}&pageSize=${encodeURIComponent(params.pagesize)}&api_key=${encodeURIComponent(params.api_key)}`
-
+// getData function that calls fetch
+// it is an asynchronous function because it needs to wait for the promised API return,
+// the promised object gives and error if accessed before the return so an addition .then() is necessary
 async function getData() {
 	let info =  await fetch(api_url).then(response => response.json())
 	return info
 }
+//initial call of getData for entire API return
 var info = await getData().then(data => {return (data).foods} )
+//API return that returns specifically the foods, eliminating the other returned information
 var nutritionInfo = await getData().then(data => {return (data).foods} )
 
 //Create map of nutrient info
 var resultsMap = new Map()
+//interates through the returned products 
 for(let i = 0; i < Object.keys(info).length; i++)
 {
 	var nutritionDict = []
+	//extracts the current iterations nutrition information
 	var nutrients = nutritionInfo[i].foodNutrients
+	//interates through each of the 10 provided nurtients and pushes them to dictionary
 	for(let j = 0; j < 10; j++)
 	{
 		nutritionDict.push({Nutrient : nutrients[j].nutrientName, Grams :  nutrients[j].value})
 	}
-
+	//pushes the product name and associated nutrition dictionary onto a map
 	resultsMap.set(info[i].brandName +', '+ info[i].description,nutritionDict)
 
 }
@@ -36,14 +41,19 @@ for(let i = 0; i < Object.keys(info).length; i++)
 
 //Create new 'simpler' map
 var resultsMap2 = new Map()
+//simplifies nutrient names so that they can be accessed later.
+//iterates through the returned products
 for(let i = 0; i < Object.keys(info).length; i++)
 {
 	var nutritionDict = {}
+	//gets that specific products nutrition info
 	var nutrients = nutritionInfo[i].foodNutrients
+	//iterates through each nutrient
 	for(let j = 0; j < 10; j++)
 	{
         var nutrient;
         var value;
+	//splits the nutrient name, removing whitespace so that it can be searched for later without error
         nutrient = ((nutrients[j].nutrientName).split(' ')[0]).split(',')[0]
         value = nutrients[j].value
 		nutritionDict[nutrient] = value   
@@ -59,56 +69,60 @@ var maxName
 var minName
 
 //Sorting checks to call sepcific functions
+//manually inputted user attributes for testing
 var sortCriteria = "Protein" //1 for most 2 for least
 var order = 'ascending'
 var sortedMap = new Map()
+//sorter function takes in nutrient to sort by and the desired order
 function sorter(sortCrtiteria,order)
 {
 	if(order == 'descending')
 	{
+		//iterates through each food
 		for(let i = 0; i <5; i++)
 	{
 		max = -1
+		//forEach function checks the value of current product and returns desired least or greatest
 		resultsMap2.forEach(function(value, key) {
+		//function provides sorting aspect checking current running max 
 		if(value[sortCriteria] > max)
 		{
+			//checks if value is greater than current running max to save for return
 			max = value[sortCriteria]
 			maxName = key
 		}
 		})
+		//pushes to new sorted map
 		sortedMap.set(maxName,resultsMap2.get(maxName))
-		//console.log("MAX IS: " + maxName + " = " + max)
+		
 		resultsMap2.delete(maxName)
 	}
 	}
 	else if(order == 'ascending')
 	{
+		//iterates through each food
 		for(let i = 0; i <5; i++)
 	{
 		min = 9999999
+		//forEach function checks the value of current product and returns desired least or greatest
 		resultsMap2.forEach(function(value, key) {
+		//function provides sorting aspect checking current running min
 		if(value[sortCriteria] < min)
 		{
+			//checks if value is less than current running min to save for return
 			min = value[sortCriteria]
 			minName = key
 		}
 		})
-		sortedMap.set(minName,resultsMap2.get(minName))
-		//console.log("MIN IS: " + minName + " = " + min)
+		//pushes to new sorted map
+		sortedMap.set(minName,resultsMap2.get(minName))	
 		resultsMap2.delete(minName)
 	}
 	}
 }
+//test call of sorting function
 sorter(sortCriteria,order)
 console.log(sortedMap);
 return sortedMap;
 }
-//Stripping the brand name when searching for alts (can do 2 text boxes)
-//Parsing out specific nutrients
-//Printing to the console: Dietz and Watson Provolone Cheese, Protein: 26, Fat: 28
 
-//Store the objects/whatever in a map
-//Either sort the map or find top alternative, 2nd alt, 3rd alt -> Return those
-
-//Storing/Sorting -> Storing in a datastructure
-//Input -> the params
