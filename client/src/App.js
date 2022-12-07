42ab5a0b989e0b274333b8325aa481baba5b6066
Andrew Ruales <67757892+andrewruales@users.noreply.github.com>
2022-12-06 21:08:39 -0500

    comments for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,13 @@ import Get from "./components/get.jsx";
  
 const App = () => {
  return (
-   <div className="bg-gray-900 min-h-screen">
+   //set background color and minimum screen height
+   <div className="bg-gray-900 min-h-screen"> 
+     //navbar - we are putting login and register in the navbar for user to click on
      <Navbar />
      <Routes>
        <Route exact path="/" />
+       //routes add  register, login, and api backend functionality to the frontend
        <Route path="/register" element={<Register />} />
        <Route path="/login" element={<Login />} />
        <Route path="/get" element={<Get />} />
